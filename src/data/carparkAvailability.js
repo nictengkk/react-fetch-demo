1f99e180c071655d1e84bb4af3670901f80f72ad
nictengkk <nictengkk@gmail.com>
2019-03-04 14:21:51 +0800

    tried axios API

--- src/data/carparkAvailability.js
+++ src/data/carparkAvailability.js
@@ -1,9 +1,24 @@
+import axios from "axios";
+
 export default async function availableCarparks() {
   const currentTime = encodeURI(new Date().toISOString());
-  const response = await fetch(
+  const response = await axios.get(
     "https://api.data.gov.sg/v1/transport/carpark-availability?date_time=" +
       currentTime
   );
-  const data = await response.json();
+
+  const data = await response.data;
   return data.items[0].carpark_data;
 }
+
+// export default async function availableCarparks() {
+//   const currentTime = encodeURI(new Date().toISOString());
+//   const response = await fetch(
+//     "https://api.data.gov.sg/v1/transport/carpark-availability?date_time=" +
+//       currentTime
+//   );
+//   console.log(response.status);
+
+//   const data = await response.json();
+//   return data.items[0].carpark_data;
+// }
